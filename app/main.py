ae151048db96cae5edbfef508d0337bbdc5595ac
RPM2626 <63054880+RPM2626@users.noreply.github.com>
2026-04-14 19:18:00 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
-import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
-from app.routers.game import router as game_router
+from app.routes import game, auth, history
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-app.include_router(game_router)
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+app.include_router(auth.router)
+app.include_router(game.router)
+app.include_router(history.router)
