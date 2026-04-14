28eb502e8afc5efb53c7126c13860fcf627a36b6
RPM2626 <63054880+RPM2626@users.noreply.github.com>
2026-04-14 14:55:41 -0400

    Update game_model.py

--- app/models/game_model.py
+++ app/models/game_model.py
@@ -1,1 +1,12 @@
+class GameResult:
+    def __init__(self, guess, bulls, cows):
+        self.guess = guess
+        self.bulls = bulls
+        self.cows = cows
 
+
+class LeaderboardEntry:
+    def __init__(self, date, attempts, status):
+        self.date = date
+        self.attempts = attempts
+        self.status = status
